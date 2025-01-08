fdb944ac6039e95d596ca32b852135efb14f124b
RichaRajveer19 <richarajveer19@gmail.com>
2025-01-08 17:46:49 +0530

    r

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ let header= document.querySelector('header');
     menu.classList.remove('fa-times');
     header.classList.remove('active');
     }
+function gotowhatsapp(){
+    var name = document.getElementById("name").Value;
+    var email = document.getElementById("email").value;
+    var msg = document.getElementById("msg").value;
+
+    alert(name);
+
+    var url = "http://wa.me/+917096449355?text="
+    + "Name: " + name + "%0a"
+    + "Email: " + email + "%0a"
+    + "Message: " + msg;
+    window.open(url).focus();
+}
