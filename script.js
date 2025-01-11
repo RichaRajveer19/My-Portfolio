b66d7839c5c5a82714526b2ea2616a7094b08af8
RichaRajveer19 <richarajveer19@gmail.com>
2025-01-11 23:18:10 +0530

    r

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 let menu = document.querySelector('#menu-bars');
-let header= document.querySelector('header');
+let header = document.querySelector('header');
 
- menu.onclick = () => {
+menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
+
 }
-    window.onscroll = () => {
+
+window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
-    }
-function gotowhatsapp(){
-    var name = document.getElementById("name").Value;
+
+}
+function gotowhatsapp() {
+    var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
+    var sub = document.getElementById("sub").value;
     var msg = document.getElementById("msg").value;
 
     alert(name);
 
-    var url = "http://wa.me/+917096449355?text="
+    var url = "https://wa.me/+919426671316?text=" 
     + "Name: " + name + "%0a"
     + "Email: " + email + "%0a"
+    + "Subject: " + sub + "%0a"
     + "Message: " + msg;
     window.open(url).focus();
 }
